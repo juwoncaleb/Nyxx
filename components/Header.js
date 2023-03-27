893fed34d58ad283f7d8d7fc9db7572e024530bc
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-03-27 23:04:57 +0100

    commit

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='flex justify-between header_content'>
+        <img className='logo' src='./logo.png' />
+        <div className='flex '>
+          <p className='mr-16 mt-6'>About</p>
+          <p className='mr-16 mt-6'>Blogs</p>
+          <p className='mr-16 mt-6'>features</p>
+          <p className='mr-16 mt-6'>Services</p>
+          <p className='mr-16 mt-6'>Lefgal</p>
+
+          <button className='nav_btn'>
+            download  aapp
+          </button>
+
+        </div>
+      </div>
+    </div>
   )
 }
