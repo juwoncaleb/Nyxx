944f8fffb83e1014730929690ec8f5536ce50d30
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-04-03 16:44:52 +0100

    working on buttons

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 export default function blog() {
+    const [activeButton, setActiveButton] = useState(null);
+    const divStyle = {
+        backgroundColor: 'blue',
+        color: 'white',
+        padding: '10px',
+    };
+
     return (
         <div className='blogsPost'>
             <Header />
@@ -36,12 +43,28 @@ export default function blog() {
                         <div className='flex '>
                             <input className='input' />
                             <button className='subs'>
-                                 Subscribe
+                                Subscribe
                             </button>
                         </div>
                     </div>
                     <img className='blog_img' src='./blog/pc.png' />
                 </div>
+            </div>
+            <div>
+                <div className='flex justify-between blog_button'>
+                    <p><span className='neon'>Latest</span> articles</p>
+                    <div>
+                        <button className='mr-6 blog_btn' onClick={setActiveButton(1); divStyle() }>Button 1</button>
+                        <button className='mr-6 blog_btn' onClick={() => setActiveButton(2)}>Button 2</button>
+                        <button className='mr-6 blog_btn' onClick={() => setActiveButton(3)}>Button 3</button>
+                        <button className='mr-6 blog_btn' onClick={() => setActiveButton(4)}>Button 4</button>
+                    </div>
+                </div>
+
+                {activeButton === 1 ? <div>Div 1: Text A</div> : null}
+                {activeButton === 2 ? <div>Div 2: Text X</div> : null}
+                {activeButton === 3 ? <div>Div 3: Text Y</div> : null}
+                {activeButton === 4 ? <div>Div 4: Text Z</div> : null}
             </div>
             <Footer />
 
