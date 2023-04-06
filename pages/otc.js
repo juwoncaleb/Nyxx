5aca13fb0f11031f3794b0398de84d796630cd61
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-04-06 17:07:09 +0100

    update

--- pages/otc.js
+++ pages/otc.js
@@ -11,18 +11,67 @@ export default function otc() {
                 <p className='otc_sub ot_s'>Are you an institution, a high volume trader or a new crypto bro, we have</p>
                 <p className='otc_sub'>designed our hassle-free OTC desk just for you</p>
                 <div class="button-container">
-                    <button class="learn mr-6">
+                    <button class="learmn mr-6">
                         Learn more
                     </button>
                 </div>
             </div>
             <div>
-                <p className='explaination'>What is Otc</p>
+                <p className='explaination'>What is OTC?</p>
                 <div className='dets'>
                     <p>Our Over-the-counter (OTC) desk is an exclusive and more personalized service outside of the Patricia</p>
                     <p>App. Where high net-worth individuals can get instant liquidity for their bulk trades, with utmost privacy</p>
                     <p>and the most competitive rates.</p>
-                    <img src='./vd.png'/>
+                    <img className='video' src='./vd.png' />
+                </div>
+            </div>
+            <div>
+                <p className='explaination'>Trade on the exchange with the <br /> most benefits</p>
+                <div className='dets'>
+                    <p>Our OTC is designed to give you value for your time and money.</p>
+                    <div className='grid md:grid-cols-2 gap-8 trade_benefit mt-14'>
+                        <div className='ot_card'>
+                            <img className='otc_icon' src='./donation.png' />
+                            <p className='otc_Card_icon'>Personalized trading experience</p>
+                            <p>Occaecat dolor incididunt excepteur aliqua officia tempor mollit veniam dolor dolore reprehenderit velit nulla irure.</p>
+
+                        </div>
+                        <div className='ot_card'>
+                            <img className='otc_icon' src='./referral.png' />
+                            <p className='otc_Card_icon'>Personalized trading experience</p>
+                            <p>Occaecat dolor incididunt excepteur aliqua officia tempor mollit veniam dolor dolore reprehenderit velit nulla irure.</p>
+
+                        </div>
+                        <div className='ot_card'>
+                            <img className='otc_icon' src='./wall.png' />
+                            <p className='otc_Card_icon'>Personalized trading experience</p>
+                            <p>Occaecat dolor incididunt excepteur aliqua officia tempor mollit veniam dolor dolore reprehenderit velit nulla irure.</p>
+
+                        </div>
+                        <div className='ot_card'>
+                            <img className='otc_icon' src='./tranfer.png' />
+                            <p className='otc_Card_icon'>Personalized trading experience</p>
+                            <p>Occaecat dolor incididunt excepteur aliqua officia tempor mollit veniam dolor dolore reprehenderit velit nulla irure.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <p className='explaination'>Trade on the exchange with the <br /> most benefits</p>
+                <div className='flex '>
+                    <div>
+                        <div>
+                            <div>
+                                <p>1</p>
+
+                            </div>
+                            <div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <Footer />
