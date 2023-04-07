c06e3bdfe5209dbf0940fd31bd40a3e0dd183470
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-04-07 12:07:06 +0100

    otc faq

--- pages/otc.js
+++ pages/otc.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 export default function otc() {
+    const [active, setActive] = useState(null)
+
     return (
         <div className='otc'>
             <Header />
@@ -58,20 +60,141 @@ export default function otc() {
             </div>
 
             <div>
-                <p className='explaination'>Trade on the exchange with the <br /> most benefits</p>
-                <div className='flex '>
-                    <div>
-                        <div>
-                            <div>
-                                <p>1</p>
+                <p className='explaination'>The OTC Desk Process</p>
+                <div className='flex justify-center  mt-20'>
+                    <div className='onBoard_texts'>
+                        <div className='flex onboard'>
+                            <div class="vl"></div>
+                            <div className='onboard_numbers'>
+                                <div className='onboard_number'>
+                                    <p>1</p>
 
+                                </div>
                             </div>
                             <div>
-                                
+                                <p className='onboarding_texts'>ONBOARD</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                            </div>
+                        </div>
+                        <div className='flex onboard '>
+                            <div class="vl"></div>
+                            <div className='onboard_numbers'>
+                                <div className='onboard_number'>
+                                    <p>2</p>
+                                </div>
+                            </div>
+                            <div >
+                                <p className='onboarding_texts'>ONBOARD</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                            </div>
+                        </div>
+                        <div className='flex onboard '>
+                            <div class="vl"></div>
+                            <div className='onboard_numbers'>
+                                <div className='onboard_number' >
+                                    <p>3</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p className='onboarding_texts'>ONBOARD</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
+                                <p>Pariatur incididunt consequat et reprehenderit cupidatat.</p>
                             </div>
                         </div>
                     </div>
-
+                    <img className='onBoard_laptop' src='./laptop.png' />
+                </div>
+            </div>
+            <div className='faq_head'>
+                <p className='faq'>FAQ</p>
+                <div>
+                    <div className='flex justify-between mt-10 mb-8'>
+                        <p className='question'>How to order Binance Card?</p>
+                        <button onClick={() => setActive(active === 1 ? null : 1)}>
+                            {active === 1 ? <p>-</p> : <p>+</p>}
+                        </button>
+                    </div>
+                    {active === 1 && (
+                        <div>
+                            <p>
+                                Veniam sint cupidatat officia Lorem enim officia commodo voluptate
+                                exercitation adipisicing incididunt minim. Reprehenderit
+                                reprehenderit culpa proident nulla elit id in ullamco ex eu amet
+                                dolor nostrud officia. Anim et id cillum ea fugiat duis occaecat.
+                                Non deserunt adipisicing do sunt et id dolore eiusmod incididunt ad
+                                est veniam dolor. Dolore non enim officia aute dolore eu irure est
+                                velit minim nostrud laboris.
+                            </p>
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <div className='flex justify-between mt-10 mb-8'>
+                        <p className='question'>How to order Binance Card?</p>
+                        <button onClick={() => setActive(active === 2 ? null : 2)}>
+                            {active === 2 ? <p>-</p> : <p>+</p>}
+                        </button>
+                    </div>
+                    {active === 2 && (
+                        <div>
+                            <p>
+                                Veniam sint cupidatat officia Lorem enim officia commodo voluptate
+                                exercitation adipisicing incididunt minim. Reprehenderit
+                                reprehenderit culpa proident nulla elit id in ullamco ex eu amet
+                                dolor nostrud officia. Anim et id cillum ea fugiat duis occaecat.
+                                Non deserunt adipisicing do sunt et id dolore eiusmod incididunt ad
+                                est veniam dolor. Dolore non enim officia aute dolore eu irure est
+                                velit minim nostrud laboris.
+                            </p>
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <div className='flex justify-between mt-10 mb-8'>
+                        <p className='question'>How to order Binance Card?</p>
+                        <button onClick={() => setActive(active === 3 ? null : 3)}>
+                            {active === 3 ? <p>-</p> : <p>+</p>}
+                        </button>
+                    </div>
+                    {active === 3 && (
+                        <div>
+                            <p>
+                                Veniam sint cupidatat officia Lorem enim officia commodo voluptate
+                                exercitation adipisicing incididunt minim. Reprehenderit
+                                reprehenderit culpa proident nulla elit id in ullamco ex eu amet
+                                dolor nostrud officia. Anim et id cillum ea fugiat duis occaecat.
+                                Non deserunt adipisicing do sunt et id dolore eiusmod incididunt ad
+                                est veniam dolor. Dolore non enim officia aute dolore eu irure est
+                                velit minim nostrud laboris.
+                            </p>
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <div className='flex justify-between mt-10 mb-8'>
+                        <p className='question'>How to order Binance Card?</p>
+                        <button onClick={() => setActive(active === 4 ? null : 4)}>
+                            {active === 4 ? <p>-</p> : <p>+</p>}
+                        </button>
+                    </div>
+                    {active === 4 && (
+                        <div>
+                            <p>
+                                Veniam sint cupidatat officia Lorem enim officia commodo voluptate
+                                exercitation adipisicing incididunt minim. Reprehenderit
+                                reprehenderit culpa proident nulla elit id in ullamco ex eu amet
+                                dolor nostrud officia. Anim et id cillum ea fugiat duis occaecat.
+                                Non deserunt adipisicing do sunt et id dolore eiusmod incididunt ad
+                                est veniam dolor. Dolore non enim officia aute dolore eu irure est
+                                velit minim nostrud laboris.
+                            </p>
+                        </div>
+                    )}
                 </div>
             </div>
             <Footer />
