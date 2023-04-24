077d2fb86add5bb99d13c8bc8949062c2eea9e0a
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-04-24 12:37:35 +0100

    webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = nextConfig
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    config.module.rules.forEach((rule) => {
+      if (rule.test && typeof rule.test.toString === 'function' && rule.test.toString().includes('.css')) {
+        rule.use.push({
+          loader: MiniCssExtractPlugin.loader,
+        }, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            modules: {
+              mode: 'local',
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+              // ...
+            },
+          },
+        });
+      }
+    });
+
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: isServer ? '[name].server.css' : '[name].css',
+      chunkFilename: isServer ? '[name].[contenthash].server.css' : '[name].[contenthash].css',
+    }));
+
+    return config;
+  },
+};
